e07804353b10b42f4e770098c8f54c184055ba98
sunnys2111 <sunnys21111999@gmail.com>
2022-08-29 12:25:13 +0530

    first commit from calcm

---
+++ src/test/java/com/anups/calcm/CalcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anups.calcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
